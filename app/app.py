bc21e3be2d54dff22cefe75587169835e08386a3
taizul <taizul@itgrowtech.com>
2024-04-30 09:54:46 +0600

    hfghfgh

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-import google.generativeai as genai
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import easyocr
+import os
 
 app = FastAPI()
 
@@ -7,6 +8,26 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/upload")
+async def root(uploaded_file: UploadFile = File(...)):
+    file_location = f"file/{uploaded_file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(uploaded_file.file.read())
+
+    h = []
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(file_location)
+
+    for (bbox, text, prob) in result:
+        h.append(text)
+        print(f'Text: {text}, Probability: {prob} {bbox}')
+
+    os.remove(file_location)
+
+    return {"result": h}
+
 
 
 def get_app():
